230bf9f70b81b8d811cb422db6f9749237bf1dac
Justin Borek <justinborek@gmail.com>
2018-01-25 13:54:51 -0600

    working

--- src/cpu.js
+++ src/cpu.js
@@ -1,3 +1,5 @@
+// import { setInterval } from 'timers';
+
 /**
  * LS-8 v2.0 emulator skeleton code
  */
@@ -7,10 +9,22 @@ const fs = require('fs');
 // Instructions
 
 const HLT  = 0b00011011; // Halt CPU
-// !!! IMPLEMENT ME
-// LDI
-// MUL
-// PRN
+const LDI  = 0b00000100; // Load Register Immediate
+const MUL  = 0b00000101; // Multiply Register Register
+const PRN  = 0b00000110; // print Register Register
+const PUSH = 0b00001010;
+const POP = 0b00001011; // Push Register
+const CALL = 0b00001111;
+const RET = 0b00010000;
+const JMP = 0b00010001;
+const ADD = 0b00001100;
+const INT = 0b00011001;
+const ST = 0b00001001;
+const IRET = 0b00011010;
+const PRA = 0b00000111;
+
+const IM = 5;
+const IS = 6;
 
 /**
  * Class for simulating a simple Computer (CPU & memory)
@@ -23,11 +37,16 @@ class CPU {
     constructor(ram) {
         this.ram = ram;
 
-        this.reg = new Array(8).fill(0); // General-purpose registers
-        
+        this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
+        this.reg[7] = 0xf8;
         // Special-purpose registers
         this.reg.PC = 0; // Program Counter
         this.reg.IR = 0; // Instruction Register
+
+        this.flags = {
+            interruptsEnabled: true,
+            overflow: false,
+        }
 
 		this.setupBranchTable();
     }
@@ -39,10 +58,19 @@ class CPU {
 		let bt = {};
 
         bt[HLT] = this.HLT;
-        // !!! IMPLEMENT ME
-        // LDI
-        // MUL
-        // PRN
+        bt[MUL] = this.MUL;
+        bt[LDI] = this.LDI;
+        bt[PRN] = this.PRN;
+        bt[PUSH] = this.PUSH;
+        bt[POP] = this.POP;
+        bt[CALL] = this.CALL;
+        bt[RET] = this.RET;
+        bt[JMP] = this.JMP;
+        bt[ADD] = this.ADD;
+        bt[INT] = this.INT;
+        bt[ST] = this.ST;
+        bt[IRET] = this.IRET;
+        bt[PRA] = this.PRA;
 
 		this.branchTable = bt;
 	}
@@ -63,6 +91,10 @@ class CPU {
         this.clock = setInterval(() => {
             _this.tick();
         }, 1);
+
+        this.timerHandle = setInterval(() => {
+            this.reg[IS] |= 0b00000001
+        }, 1000);
     }
 
     /**
@@ -78,9 +110,15 @@ class CPU {
      * op can be: ADD SUB MUL DIV INC DEC CMP
      */
     alu(op, regA, regB) {
+        let valA = this.reg[regA];
+        let valB = this.reg[regB];
+
         switch (op) {
             case 'MUL':
-                // !!! IMPLEMENT ME
+                this.reg[regA] = (valA * valB) & 255;
+                break;
+            case 'ADD':
+                this.reg[regA] = (valA + valB) & 255;
                 break;
         }
     }
@@ -89,18 +127,52 @@ class CPU {
      * Advances the CPU one cycle
      */
     tick() {
-        // !!! IMPLEMENT ME
+        // Interrupt stuff
+        const maskedInterrupts = this.reg[IS] & this.reg[IM];
+        
+        if (this.flags.interruptsEnabled && maskedInterrupts !== 0) {
+            for (let i = 0; i <= 7; i++) {
+                if (((maskedInterrupts >> i) & 1) === 1) {
+                    this.flags.interruptsEnabled = false;
 
-        // Load the instruction register from the current PC
+                    this.reg[IS] &= ~(1 << i);
+
+                    this.reg[7]--;
+                    this.ram.write(this.reg[7], this.reg.PC);
+
+                    for (let j = 0; j <= 7; j++) {
+                        this.reg[7]--;
+                        this.ram.write(this.reg[7], this.reg[j]);
+                    }
+
+                    const vectorTableEntry = 0xf8 + i;
+                    const handlerAddress = this.ram.read(vectorTableEntry);
+
+                    this.reg.PC = handlerAddress;
+
+                    break;
+                }
+            }
+        }
+        
+        // Load the instruction register from the memory address pointed to by
+        // the PC
+        this.reg.IR = this.ram.read(this.reg.PC);
 
         // Debugging output
-        //console.log(`${this.reg.PC}: ${this.reg.IR.toString(2)}`);
+        // console.log(`${this.reg.PC}: ${this.reg.IR.toString(2)}`);
 
         // Based on the value in the Instruction Register, jump to the
         // appropriate hander in the branchTable
+        const handler = this.branchTable[this.reg.IR];
 
         // Check that the handler is defined, halt if not (invalid
         // instruction)
+        if (!handler) {
+            console.error(`Invalid instruction at address ${this.reg.PC}: ${this.reg.IR.toString(2)}`);
+            this.stopClock();
+            return;
+        }
 
         // We need to use call() so we can set the "this" value inside
         // the handler (otherwise it will be undefined in the handler)
@@ -113,28 +185,116 @@ class CPU {
      * HLT
      */
     HLT() {
-        // !!! IMPLEMENT ME
+        this.stopClock();
     }
 
     /**
      * LDI R,I
      */
     LDI() {
-        // !!! IMPLEMENT ME
+        const regA = this.ram.read(this.reg.PC + 1);
+        const val = this.ram.read(this.reg.PC + 2);
+
+        this.reg[regA] = val;
+
+        this.reg.PC += 3;
     }
 
     /**
      * MUL R,R
      */
     MUL() {
-        // !!! IMPLEMENT ME
+        const regA = this.ram.read(this.reg.PC + 1);
+        const regB = this.ram.read(this.reg.PC + 2);
+
+        this.alu('MUL', regA, regB);
+
+        // Move the PC
+        this.reg.PC += 3;
     }
 
     /**
      * PRN R
      */
     PRN() {
-        // !!! IMPLEMENT ME
+        const regA = this.ram.read(this.reg.PC + 1);
+
+        console.log(this.reg[regA]);
+
+        this.reg.PC += 2;
+    }
+
+    PUSH() {
+        const regA = this.ram.read(this.reg.PC + 1);
+        this.reg[7]--;
+        this.ram.write(this.reg[7], this.reg[regA]);
+        this.reg.PC += 2;
+    }
+
+    POP() {
+        const regA = this.ram.read(this.reg.PC + 1);
+        const stackVal = this.ram.read(this.reg[7]);
+        this.reg[regA] = stackVal;
+        this.reg[7]++;
+        this.reg.PC += 2;
+    }
+
+    CALL() {
+        const regA = this.ram.read(this.reg.PC + 1);
+
+        this.reg[7]--;
+
+        this.ram.write(this.reg[7], this.reg.PC + 2);
+        this.reg.PC = this.reg[regA];
+    }
+
+    RET() {
+        this.reg.PC = this.ram.read(this.reg[7]);
+        this.reg[7]++;
+    }
+
+    JMP() {
+        const regA = this.ram.read(this.reg.PC + 1);
+
+        this.reg.PC = this.reg[regA];
+    }
+
+    ADD() {
+        const regA = this.ram.read(this.reg.PC + 1);
+        const regB = this.ram.read(this.reg.PC + 2);
+
+        this.alu('ADD', regA, regB);
+
+        this.reg.PC += 3;
+    }
+
+    ST() {
+        const regA = this.ram.read(this.reg.PC + 1);
+        const regB = this.ram.read(this.reg.PC + 2);
+
+        this.ram.write(this.reg[regA], this.reg[regB]);
+
+        this.reg.PC += 3;
+    }
+    
+    IRET() {
+        for (let j = 7; j >= 0; j--) {
+            this.reg[j] = this.ram.read(this.reg[7]);
+            this.reg[7]++;
+        }
+        
+        this.reg.PC = this.ram.read(this.reg[7]);
+        this.reg[7]++;
+        
+        this.flags.interruptsEnabled = true;
+    }
+
+    PRA() {
+        const regA = this.ram.read(this.reg.PC + 1);
+
+        console.log(String.fromCharCode(this.reg[regA]));
+
+        this.reg.PC;
     }
 }
 
--- src/ram.js
+++ src/ram.js
@@ -11,18 +11,17 @@ class RAM {
      * Write (store) MDR value at address MAR
      */
     write(MAR, MDR) {
-        // !!! IMPLEMENT ME
         // write the value in the MDR to the address MAR
+        this.mem[MAR] = MDR;
     }
 
     /**
      * Read (load) MDR value from address MAR
-     * 
-     * @returns MDR
+     *
      */
     read(MAR) {
-        // !!! IMPLEMENT ME
         // Read the value in address MAR and return it
+        return this.mem[MAR];
     }
 }
 
